76a23f1e7bc8e7651575c1c9aab8076c82693045
Matyáš Sýs <sysm22@spstrutnov.cz>
2024-10-11 17:51:15 +0200

    Added PostController and AuthHelper, modified Create Post form

---
+++ Source/Jobinator/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using Jobinator.Data;
+using Jobinator.Helpers;
+using Jobinator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jobinator.Controllers
+{
+    public class PostController : Controller
+    {
+        private DataContext? _Data;
+        public PostController(DataContext Data)
+        {
+            _Data = Data;
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            // Create an instance of the AuthHelper manually
+            AuthHelper authHelper = new();
+
+            // Pass the DataContext and HttpContext to the helper method
+            User? LoggedUser = authHelper.GetLoggedInUser(_Data, HttpContext);
+
+            if (LoggedUser == null) return RedirectToAction("Login", "User");
+
+            return RedirectToAction("Profile", "User");
+        }
+    }
+}
