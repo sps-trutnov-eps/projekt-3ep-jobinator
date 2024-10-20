a594270af59c4150a1c3d224e8faa8966839b346
Matyáš Sýs <sysm22@spstrutnov.cz>
2024-10-20 21:22:55 +0200

    Basic AdminController and view

---
+++ Source/Jobinator/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Jobinator.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IConfiguration _config;
+        public AdminController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            //Load creds from config
+            string adminUsername = _config["AdminCredentials:Username"];
+            string adminPassword = _config["AdminCredentials:Password"];
+
+            // Compare username and password
+            if (username == adminUsername && password == adminPassword)
+            {
+                Debug.WriteLine("Login successful");
+            }
+            else
+            {
+                Debug.WriteLine("Login failed");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
