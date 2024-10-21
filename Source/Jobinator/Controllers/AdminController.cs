a12e00b63d85619ae9e73260e9a9301dc57cc9b8
Matyáš Sýs <sysm22@spstrutnov.cz>
2024-10-21 19:55:32 +0200

    Basic admin dashboard

--- Source/Jobinator/Controllers/AdminController.cs
+++ Source/Jobinator/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Jobinator.Data;
+using Jobinator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Jobinator.Controllers
@@ -6,9 +9,11 @@ namespace Jobinator.Controllers
     public class AdminController : Controller
     {
         private readonly IConfiguration _config;
-        public AdminController(IConfiguration config)
+        private DataContext? _Data;
+        public AdminController(IConfiguration config, DataContext Data)
         {
             _config = config;
+            _Data = Data;
         }
 
         public IActionResult Index()
@@ -20,20 +25,34 @@ namespace Jobinator.Controllers
         public IActionResult Login(string username, string password)
         {
             //Load creds from config
-            string adminUsername = _config["AdminCredentials:Username"];
-            string adminPassword = _config["AdminCredentials:Password"];
+            string AdminUsername = _config["AdminCredentials:Username"];
+            string AdminPassword = _config["AdminCredentials:Password"];
 
             // Compare username and password
-            if (username == adminUsername && password == adminPassword)
+            if (username == AdminUsername && password == AdminPassword)
             {
                 Debug.WriteLine("Login successful");
+                //Use the session to store that user is logged in as admin
+                HttpContext.Session.SetString("Admin", "true");
             }
             else
             {
                 Debug.WriteLine("Login failed");
             }
 
-            return RedirectToAction("Index");
+            return RedirectToAction("Dashboard");
+        }
+
+        public IActionResult Dashboard()
+        {
+            // Verify user is admin
+            if (HttpContext.Session.GetString("Admin") != "true")
+            {
+                return RedirectToAction("Index");
+            }
+            //Load all posts
+            List<Post> posts = _Data.Posts.Include(p => p.User).ToList() ?? new List<Post>();
+            return View(posts); // Pass the 'posts' list into the view
         }
     }
 }
