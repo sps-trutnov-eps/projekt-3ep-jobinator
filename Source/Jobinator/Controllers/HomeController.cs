03cf879ea474ed0e192e9ce51f00b40cf5121b01
Jakub Tryzna <tryznaj22@spstrutnov.cz>
2024-10-21 18:45:57 +0200

    Displaying posts by offer/demand

--- Source/Jobinator/Controllers/HomeController.cs
+++ Source/Jobinator/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Jobinator.Data;
 using Jobinator.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using System.Diagnostics;
 using System.Diagnostics.Eventing.Reader;
 using System.Runtime.Versioning;
@@ -32,20 +33,32 @@ namespace Jobinator.Controllers
         [HttpPost]
         public IActionResult Filter()
         {
-            var posts = _Data.Posts.ToList();
-            var filteredPosts = new List<Post>(); // a list that will contain all the posts that pass through the filter
-            string filteredCategory = Request.Form["filter"].ToString(); // saves the selected value in the filter 
+            var posts = _Data.Posts.ToList(); // a list that will contain all the posts that pass through the filter
+            var removedPosts = new List<Post>(); // list where all the posts that failed the filter will be stored
+            string filteredCategory = Request.Form["filterCategory"].ToString(); // saves the selected value in the category filter 
+            string filteredType = Request.Form["filterType"].ToString();  // saves the selected value in the type filter 
 
-            if (!string.IsNullOrEmpty(filteredCategory)) // checks if the filter isnt empty
-            { 
-                foreach (var post in posts)
+            foreach (var post in posts) {
+                if (!string.IsNullOrEmpty(filteredCategory))  // checks if a filter is selected
                 {
-                    if (Convert.ToString(post.Category).Equals(filteredCategory)) filteredPosts.Add(post); // checks if the post category is the same as the filtered one
+                    if (!Convert.ToString(post.Category).Equals(filteredCategory)) // checks if the post category is the same as the filtered one
+                    {
+                        removedPosts.Add(post); //adds the posts that didnt pass the filter
+                    }
+                }
+                if (!string.IsNullOrEmpty(filteredType)) // checks if a filter is selected
+                {
+                    if (!Convert.ToString(post.Type).Equals(filteredType)) // checks if the post category is the same as the filtered one
+                    {
+                        removedPosts.Add(post); //adds the posts that didnt pass the filter
+                    }
                 }
             }
-            else return View("Index", posts); // if no filter is selected it returns all posts
+            foreach (var post in removedPosts) {
+                posts.Remove(post); //removes the posts from the main list
+            }
 
-        return View("Index", filteredPosts); // returns the filtered posts
+        return View("Index", posts); // returns the filtered posts
         }
 
         public IActionResult Offer()
